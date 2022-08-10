68e0d1cdafeade310df1f1894fd96e6eea6b813a
Pimenta <mateuspimartins@hotmail.com>
2022-08-10 17:07:48 -0300

    branch22

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import prisma from "./database.js";
 
 const app = express();
 app.use(json());
-console.log("teste")
+console.log("teste2222")
 
 
 app.get("/students", async (req: Request, res: Response) => {
